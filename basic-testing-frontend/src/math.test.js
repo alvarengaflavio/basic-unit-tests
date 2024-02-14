2d9aa655a7241651548ed95378524af6203f95b9
alvalenda <banysan@msn.com>
2024-02-14 18:54:55 -0300

    Adiciona testes para a função add() no arquivo math.test.js

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -9,4 +9,16 @@ it("deve somar todos os valores numéricos de um array", () => {
   const numbers2 = [-1, 2, -3, 4, -5, 6];
   const result2 = add(numbers2);
   expect(result2).toBe(3);
+
+  const numbers3 = [1, 2, 3, 4, 5, -15];
+  const result3 = add(numbers3);
+  expect(result3).toBe(0);
+
+  const numbers4 = [0, 0, 0, 0, 0, 0];
+  const result4 = add(numbers4);
+  expect(result4).toBe(0);
+
+  const numbers5 = ["1", 2, 3, 4, 5];
+  const result5 = add(numbers5);
+  expect(result5).toStrictEqual(Error("Todos os valores devem ser numéricos"));
 });
