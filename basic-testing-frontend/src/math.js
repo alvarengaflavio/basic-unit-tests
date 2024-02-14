ffe5f051fccce96524abeaa26f8cd249751febe3
alvalenda <banysan@msn.com>
2024-02-14 18:54:49 -0300

    Adiciona validação para garantir que todos os valores sejam numéricos

--- basic-testing-frontend/src/math.js
+++ basic-testing-frontend/src/math.js
@@ -2,7 +2,12 @@ export function add(numbers) {
   let sum = 0;
 
   for (const number of numbers) {
+    if (typeof number !== "number") {
+      return new Error("Todos os valores devem ser numéricos");
+    }
+
     sum += number;
   }
+
   return sum;
 }
