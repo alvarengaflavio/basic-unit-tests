dad1c81cb88201691e78bd793bc1e909ddd3f921
alvalenda <banysan@msn.com>
2024-02-14 16:38:12 -0300

    Atualizar testes de soma de números em um array

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -3,12 +3,10 @@ import { add } from "./math";
 
 it("deve somar todos os valores numéricos de um array", () => {
   const numbers = [1, 2, 3, 4, 5];
-  const expected = 15;
   const result = add(numbers);
-  expect(result).toBe(expected);
+  expect(result).toBe(15);
 
   const numbers2 = [-1, 2, -3, 4, -5, 6];
-  const expected2 = 3;
   const result2 = add(numbers2);
-  expect(result2).toBe(expected2);
+  expect(result2).toBe(3);
 });
