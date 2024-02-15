342c8bd4147f4490e2faa50cfc400cdc6dc6605c
alvalenda <banysan@msn.com>
2024-02-15 11:00:29 -0300

    Altera testes na função de soma em math.test.js para seguir `AAA pattern`

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -3,22 +3,32 @@ import { add } from "./math";
 
 it("deve somar todos os valores numéricos de um array", () => {
   const numbers = [1, 2, 3, 4, 5];
+  const expected = 15;
   const result = add(numbers);
-  expect(result).toBe(15);
+  expect(result).toBe(expected);
 
   const numbers2 = [-1, 2, -3, 4, -5, 6];
+  const expected2 = 3;
   const result2 = add(numbers2);
-  expect(result2).toBe(3);
+  expect(result2).toBe(expected2);
 
   const numbers3 = [1, 2, 3, 4, 5, -15];
+  const expected3 = 0;
   const result3 = add(numbers3);
-  expect(result3).toBe(0);
+  expect(result3).toBe(expected3);
 
   const numbers4 = [0, 0, 0, 0, 0, 0];
+  const expected4 = 0;
   const result4 = add(numbers4);
-  expect(result4).toBe(0);
+  expect(result4).toBe(expected4);
 
-  const numbers5 = ["1", 2, 3, 4, 5];
+  const numbers5 = [1, "2", 3, 4, 5];
+  const expected5 = Error("Todos os valores devem ser numéricos");
   const result5 = add(numbers5);
-  expect(result5).toStrictEqual(Error("Todos os valores devem ser numéricos"));
+  expect(result5).toStrictEqual(expected5);
+
+  const numbers6 = [null, 2, 3, 4, 5];
+  const expected6 = Error("Todos os valores devem ser numéricos");
+  const result6 = add(numbers6);
+  expect(result6).toStrictEqual(expected6);
 });
